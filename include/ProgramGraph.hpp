b5abdf8c8c94d74dd8e9407306c25c924132be65
Sam Foucart <samfoucart25@gmail.com>
2020-10-25 18:10:03 -0400

    added drawable abstract class

--- include/ProgramGraph.hpp
+++ include/ProgramGraph.hpp
@@ -1,6 +1,11 @@
-#include "Angel.h"
+#ifndef __PROGRAM_GRAPH_HPP__
+#define __PROGRAM_GRAPH_HPP__
+
 #include <memory>
+#include <vector>
+
 #include "drone.h"
+#include "Angel.h"
 
 extern std::stack<mat4> mvStack;
 extern mat4 modelView;
@@ -11,20 +16,25 @@ struct DrawableGraph {
     void traverse(std::unique_ptr<DrawableNode>);
 };
 
-struct DrawableNode {
-    mat4 transformation;
-    virtual void draw(void) = 0;
+struct DrawableNode {   
+    std::unique_ptr<Drawable> model;
     std::unique_ptr<DrawableNode> sibling;
     std::unique_ptr<DrawableNode> child;
 };
+
+struct Drawable {
+    mat4 transformation;
+    virtual void draw() = 0;
+    std::vector<vec4> points;
+};    
 
 inline void DrawableGraph::traverse(std::unique_ptr<DrawableNode>) {
     if (root == NULL) {
         return;
     }
     mvStack.push(modelView);
-    modelView = modelView * (root->transformation);
-    root->draw();
+    modelView = modelView * (root->model->transformation);
+    root->model->draw();
     traverse(root->child);
     modelView = mvStack.top();
     mvStack.pop();
@@ -32,3 +42,5 @@ inline void DrawableGraph::traverse(std::unique_ptr<DrawableNode>) {
 }
 
 }
+
+#endif
